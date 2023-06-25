42f3c959df9dcf08698f226312de6c7c5ead6164
justin asselin <assj2605@usherbrooke.ca>
2023-06-24 20:08:24 -0400

    sync

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 [![GitHub releases](https://img.shields.io/github/v/release/juste-injuste/Katagrafeas.svg)](https://github.com/juste-injuste/Katagrafeas/releases)
 [![License](https://img.shields.io/github/license/juste-injuste/Katagrafeas.svg)](LICENSE)
 
+Katagrafeas is a simple and lightweight C++11 (and newer) library that allows you easily redirect streams towards a common destination.
+
 ## Usage
+
+Katagrafeas offers two ways to measure elapsed time:
+* [execution_time](#execution_time) function
+* [CHRONOMETRO_EXECUTION_TIME](#CHRONOMETRO_EXECUTION_TIME) macro
 
 ---
 
@@ -11,9 +17,23 @@
 
 Katagrafeas is a header-only library. To use it in your C++ project, simply `#include` the [Katagrafeas.hpp](include/Katagrafeas.hpp) header file.
 
-## Limitations
+## Disclosure
 
-## Version History
+
+## Details
+
+
+## Version
+
+The current version defines the following macros:
+```cpp
+#define KATAGRAFEAS_VERSION       001000000L
+#define KATAGRAFEAS_VERSION_MAJOR 1
+#define KATAGRAFEAS_VERSION_MINOR 0
+#define KATAGRAFEAS_VERSION_PATCH 0
+```
+
+### History
 
 Version 1.0: initial release
 
--- include/Katagrafeas.hpp
+++ include/Katagrafeas.hpp
@@ -1,55 +1,59 @@
-// --author------------------------------------------------------------------------------
-// 
-// Justin Asselin (juste-injuste)
-// justin.asselin@usherbrooke.ca
-// https://github.com/juste-injuste/Katagrafeas
-// 
-// --liscence----------------------------------------------------------------------------
-// 
-// MIT License
-// 
-// Copyright (c) 2023 Justin Asselin (juste-injuste)
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//  
-// --versions----------------------------------------------------------------------------
-//
-// version 1.0 initial release
-//
-// --description-------------------------------------------------------------------------
-//
-// --inclusion guard---------------------------------------------------------------------
+/*---author----------------------------------------------------------------------------------------
+
+Justin Asselin (juste-injuste)
+justin.asselin@usherbrooke.ca
+https://github.com/juste-injuste/Katagrafeas
+
+-----liscence--------------------------------------------------------------------------------------
+ 
+MIT License
+
+Copyright (c) 2023 Justin Asselin (juste-injuste)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ 
+-----versions--------------------------------------------------------------------------------------
+
+version 1.0 initial release
+
+-----description-----------------------------------------------------------------------------------
+
+Katagrafeas is a simple and lightweight C++11 (and newer) library that allows you easily redirect
+streams towards a common destination. See the included README.MD file for more information.
+
+-----inclusion guard-----------------------------------------------------------------------------*/
 #ifndef KATAGRAFEAS_HPP
 #define KATAGRAFEAS_HPP
-// --necessary standard libraries--------------------------------------------------------
+//---necessary standard libraries------------------------------------------------------------------
 #include <iosfwd>
 #include <iostream>
 #include <fstream>
 #include <vector>
-// --Katagrafeas library-----------------------------------------------------------------
-namespace Katagrafeas {
+//---Katagrafeas library---------------------------------------------------------------------------
+namespace Katagrafeas
+{
   // library version
   #define KATAGRAFEAS_VERSION       001000000L
   #define KATAGRAFEAS_VERSION_MAJOR 1
   #define KATAGRAFEAS_VERSION_MINOR 0
   #define KATAGRAFEAS_VERSION_PATCH 0
-// --Katagrafeas library: frontend forward declarations----------------------------------
+//---Katagrafeas library: frontend forward declarations--------------------------------------------
   inline namespace Frontend
   {
     // default logger
@@ -58,16 +62,17 @@ namespace Katagrafeas {
     // create logger with custom stream names
     #define KATAGRAFEAS_MAKELOGGER(...)
   }
-// --Katagrafeas library: backend struct and class definitions---------------------------
+//---Katagrafeas library: backend struct and class definitions-------------------------------------
   namespace Backend
   {
     class Stream final {
       public:
         inline Stream(std::ostream& stream) noexcept;
+        // output to stream directly
         template<typename T>
-        inline std::ostream& operator<<(const T& text) const noexcept;
-        // redirect stream (and save its original buffer)
-        inline void redirect(std::ostream& stream) const noexcept;
+        inline const Stream& operator<<(const T& text) const noexcept;
+        // redirect stream (and backup its original buffer)
+        inline void link(std::ostream& stream) const noexcept;
         // restore stream's original buffer
         inline void restore(std::ostream& stream) const noexcept;
       private:
@@ -77,55 +82,51 @@ namespace Katagrafeas {
         };
         // streams and their original buffer
         mutable std::vector<Backup> backups_;
-        // destination stream
+        // output stream
         std::ostream& stream_;
     };
   }
-// --Katagrafeas library: frontend struct and class definitions--------------------------
+//---Katagrafeas library: frontend definitions-----------------------------------------------------
   inline namespace Frontend
   {
     struct Logger final {
-      inline Logger(void) noexcept;
-      inline Logger(std::ostream& out, std::ostream& err, std::ostream& wrn, std::ostream& log) noexcept;
+      inline Logger(std::ostream& out = std::cout,
+                    std::ostream& err = std::cerr,
+                    std::ostream& wrn = std::cerr,
+                    std::ostream& log = std::clog,
+                    std::ostream& dbg = std::clog) noexcept;
+      // output stream
       const Backend::Stream out;
+      // error stream
       const Backend::Stream err;
+      // warning stream
       const Backend::Stream wrn;
+      // general log stream
       const Backend::Stream log;
+      // debug stream
+      const Backend::Stream dbg;
     };
-  }
-// --Katagrafeas library: frontend struct and class member definitions-------------------
-  inline namespace Frontend
-  {
-    Logger::Logger(void) noexcept
-      : // member initialization list
-      out(std::cout),
-      err(std::cerr),
-      wrn(std::cerr),
-      log(std::clog)
-    {}
 
-    Logger::Logger(std::ostream& out, std::ostream& err, std::ostream& wrn, std::ostream& log) noexcept
+    Logger::Logger(std::ostream& out, std::ostream& err, std::ostream& wrn,
+                   std::ostream& log, std::ostream& dbg) noexcept
       : // member initialization list
       out(out),
       err(err),
       wrn(wrn),
-      log(log)
+      log(log),
+      dbg(dbg)
     {}
-  }
-// --Katagrafeas library: frontend definitions-------------------------------------------
-  inline namespace Frontend
-  {
+
     #undef  KATAGRAFEAS_MAKELOGGER
-    #define KATAGRAFEAS_MAKELOGGER(...)       \
-      []{                                     \
-        using namespace Katagrafeas::Backend; \
-        struct Logger {                       \
-          const Stream __VA_ARGS__;           \
-        } logger;                             \
-        return logger;                        \
+    #define KATAGRAFEAS_MAKELOGGER(...)      \
+      []{                                    \
+        struct Logger {                      \
+          const Backend::Stream __VA_ARGS__; \
+        } logger;                            \
+        return logger;                       \
       }()
   }
-// --Katagrafeas library: backend struct and class member definitions--------------------
+//---Katagrafeas library: backend struct and class member definitions------------------------------
   namespace Backend
   {
     Stream::Stream(std::ostream& stream) noexcept
@@ -134,13 +135,15 @@ namespace Katagrafeas {
     {}
 
     template<typename T>
-    std::ostream& Stream::operator<<(const T& text) const noexcept
+    const Stream& Stream::operator<<(const T& text) const noexcept
     {
       // output to stream
-      return stream_ << text;
+      stream_ << text;
+      
+      return *this;
     }
 
-    void Stream::redirect(std::ostream& stream) const noexcept
+    void Stream::link(std::ostream& stream) const noexcept
     {
       // backup stream 
       backups_.push_back(Backup{&stream, stream.rdbuf()});
@@ -153,7 +156,7 @@ namespace Katagrafeas {
     {
       // traverse backups backwards
       for(size_t i = backups_.size() - 1; i < backups_.size(); --i) {
-        // find stream in list
+        // find stream in backups
         if (backups_[i].stream == &stream) {
           // restore buffer
           stream.rdbuf(backups_[i].buffer);
@@ -161,10 +164,11 @@ namespace Katagrafeas {
           // remove from backup list
           backups_.erase(backups_.begin() + i);
 
+          // stream was restored, no more work is necessary
           return;
         }
       }
-      
+
       // if we end up here it's because the stream is not part of the backup list
       std::cerr << "error: Stream: couldn't restore stream; stream not found in backups\n";
     }
